4e0b4c6d71b45fea26363d54e12b8f2ef19bd091
RubinhoGomes <ruben.gomes16@hotmail.com>
2025-05-09 10:20:29 +0100

    Added basic configuration to start the project

--- src/schedule.js
+++ src/schedule.js
@@ -24,18 +24,52 @@ $.schedule = function(options) {
   getDefaults: function () {
     return this.DEFAULTS;
   },
- 
+
   DEFAULTS: {
     titlle: 'Schedule',
     locale: 'pt-PT',
     url: null,
     defaultColor: 'primary',
-    formatter: {
-      info: formatterInfo
+    hoursArray: {
+      start: '08:00',
+      end: '18:00',
+      interval: 30,
     },
+    header: [
+      { 'name': 'Segunda' },
+      { 'name': 'Terça' },
+      { 'name': 'Quarta' },
+      { 'name': 'Quinta' },
+      { 'name': 'Sexta' },
+      { 'name': 'Sabado' },
+      { 'name': 'Domingo' },
+    ],
+  },
+
+  utils: {
+    calculateInterval: function (time, interval) => {
+      const [hour, minute] = time.split(':').map(Number);
+      const date = new Date();
+      date.setHours(hour);
+      date.setMinutes(minute + interval);
+      return date.toTimeString().slice(0, 5);
+    },
+
+      getScheduleElement: function (event, hour) => {
+        $('.schedule').find(hour . event.id) => {
+          return $(this);
+        }
+      },
+
+    // Implement a IsDarkColor function
+    // Implement a getColor function
+
   }
 
-  
+
+
+
+
 
 }
 
